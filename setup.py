c52e47931a5e40fb26756c91a28b8a5a6affb19b
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2025-07-17 22:28:47 +0530

    the packages installed

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function reads the requirements file and returns a list of packages.
+    """
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name='ml_project',
+    version='0.1.0',
+    author='Kaushik',
+    author_email='kaushikgowda547@gmail.com',
+    description='A machine learning project',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
