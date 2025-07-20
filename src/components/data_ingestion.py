d60c040ad07cdfc04736fae0236bbcfcf92f09eb
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2025-07-20 21:42:48 +0530

    training model data

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import pandas as pd
+
 from src.exception import CustomException
 from src.logger import logging
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModeltrainerConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -56,3 +59,6 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
